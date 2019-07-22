7c706412bd560befedead502a2ffeb1326b54073
Steen Sia <steen.sia@selecthealth.org>
2019-07-22 17:55:36 -0600

    Started " Linking to Routes"

--- Fundamentals/src/app/app.module.ts
+++ Fundamentals/src/app/app.module.ts
@@ -7,15 +7,20 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.components';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent
   ],
   providers: [EventService, ToastrService],
--- Fundamentals/src/app/events-app.component.ts
+++ Fundamentals/src/app/events-app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'events-app',
   template: `
     <navbar></navbar>
-    <events-list></events-list>
+    <router-outlet></router-outlet>
   `
 })
 export class EventsAppComponent {
