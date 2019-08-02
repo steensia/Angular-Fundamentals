105d863d475ef5f83e4d3d86ac85ba1ee5be86b8
Steen Sia <steen.sia@selecthealth.org>
2019-08-02 12:36:25 -0600

    Added test cases to SessionListComponentIsolated
    ngOnChanges - shoulder filter beginner, advanced or all sessions by name or votes

--- Fundamentals/src/app/events/event-details/session-list.component.isolated.spec.ts
+++ Fundamentals/src/app/events/event-details/session-list.component.isolated.spec.ts
@@ -12,7 +12,38 @@ describe("SessionListComponentIsolated", () => {
 
     describe("ngOnChanges", () => {
 
-        it("should filter the sessions correctly", () => {
+        it("should filter all sessions correctly", () => {
+            component.sessions = [
+                {name: "session 1", level: "advanced", voters: ["1", "2"]},
+                {name: "session 2", level: "intermediate", voters: ["3", "4"]},
+                {name: "session 3", level: "beginner", voters: ["5", "6"]},
+                {name: "session 4", level: "advanced", voters: ["7", "8"]}
+            ] as ISession[];
+            component.filterBy = "all";
+            component.sortBy = "votes";
+            component.eventId = 3;
+
+            component.ngOnChanges();
+
+            expect(component.visibleSessions.length).toBe(4);
+        });
+
+        it("should filter the beginner sessions correctly", () => {
+            component.sessions = [
+                {name: "session 1", level: "intermediate"},
+                {name: "session 2", level: "intermediate"},
+                {name: "session 3", level: "beginner"}
+            ] as ISession[];
+            component.filterBy = "beginner";
+            component.sortBy = "name";
+            component.eventId = 3;
+
+            component.ngOnChanges();
+
+            expect(component.visibleSessions.length).toBe(1);
+        });
+
+        it("should filter the intermediate sessions correctly", () => {
             component.sessions = [
                 {name: "session 1", level: "intermediate"},
                 {name: "session 2", level: "intermediate"},
@@ -20,6 +51,22 @@ describe("SessionListComponentIsolated", () => {
             ] as ISession[];
             component.filterBy = "intermediate";
             component.sortBy = "name";
+            component.eventId = 3;
+
+            component.ngOnChanges();
+
+            expect(component.visibleSessions.length).toBe(2);
+        });
+
+        it("should filter the advanced sessions correctly", () => {
+            component.sessions = [
+                {name: "session 1", level: "advanced", voters: ["1", "2"]},
+                {name: "session 2", level: "intermediate"},
+                {name: "session 3", level: "beginner"},
+                {name: "session 4", level: "advanced", voters: ["3", "4"]}
+            ] as ISession[];
+            component.filterBy = "advanced";
+            component.sortBy = "votes";
             component.eventId = 3;
 
             component.ngOnChanges();
